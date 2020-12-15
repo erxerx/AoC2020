aa4f21ca6478349dcda73df92375fdba7cf3355c
erx <ervin.hylp@gmail.com>
2020-12-15 21:13:40 +0200

    ver1. works correctly but slow

--- d15_1.py
+++ d15_1.py
@@ -2,12 +2,13 @@
 #  content = f.read()
 #l = content.split('\n')
 #ll = [x.replace('= ', '').split(' ') for x in l]  #restore from file
-mem = [0,3,6]
+mem = [5,1,9,18,13,8,0]
 #mem = {}
-for turn in range(len(mem),2019):
+for turn in range(len(mem),2021):
   last = mem[turn-1]
   if mem.count(last) == 1:
     mem.append(0)
     continue
-  mem.append(turn - mem[:-1:-1].index(last) - 1)
+  a = mem[-2::-1]
+  mem.append(mem[-2::-1].index(last) + 1)
 print(last)
