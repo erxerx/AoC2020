9a9022463315d0656725ddfa7c6a7dcf85b2468b
erx <ervin.hylp@gmail.com>
2020-12-16 21:52:36 +0200

    ver1.6 wip with dict

--- d15_1.py
+++ d15_1.py
@@ -1,27 +1,21 @@
-mem = [0,3,6]
-#mem = [5,1,9,18,13,8,0]
-memdict = {mem[i]:i for i in range(len(mem)-1)}
-#mem = {}
-last = mem[len(mem)-1]
+mem = [0, 3, 6]
+# mem = [5,1,9,18,13,8,0]
+seen = {mem[i]: i for i in range(len(mem) - 1)}
+# mem = {}
+last = mem[len(mem) - 1]
 mem.pop()
-for turn in range(len(memdict),2021):
-  mem.append(last)
-  if last == 0:
-    memdict[turn - lastzero] = turn
-    last = turn - lastzero
-  elif last in memdict:
-    memdict[last] = turn - memdict[last]
-    last = turn - memdict[last]
-  else:
-    memdict[last] = turn
-    lastzero = memdict[0]
-    memdict[0] = turn + 1
-    last = 0
-  continue
-  if mem.count(last) == 1:
-    mem.append(0)
-    continue
-  a = mem[-2::-1]
-  last = newnum
-  mem.append(mem[-2::-1].index(last) + 1)
+for turn in range(len(seen), 2021):
+    mem.append(last)
+    if last == next:
+        next = 1
+    if last == 0:
+      next = turn - lastzeroturn
+    elif last in seen:  # already seen number, next is diff with last seen turn
+      next = turn - lastseen
+    else:  # first time this number, next is zero. preserve last seen 0 turn
+      next = 0
+      lastzeroturn = seen[0]
+    lastseen = seen[next]
+    seen[next] = turn
+    last = next
 print(last)
