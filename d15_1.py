05aad2dd467a38cb94c40113e9cdc9dbaed1f666
erx <ervin.hylp@gmail.com>
2020-12-16 23:48:35 +0200

    ver1.15.1 fixup

--- d15_1.py
+++ d15_1.py
@@ -1,21 +1,16 @@
-mem = [0, 3, 6]
-# mem = [5,1,9,18,13,8,0]
+#mem = [0, 3, 6]
+mem = [5,1,9,18,13,8,0]
 seen = {mem[i]: i for i in range(len(mem) - 1)}
-# mem = {}
-last = mem[len(mem) - 1]
-mem.pop()
-for turn in range(len(seen), 2021):
-    mem.append(last)
-    if last == next:
-        next = 1
-    if last == 0:
-      next = turn - lastzeroturn
-    elif last in seen:  # already seen number, next is diff with last seen turn
+lastseen = len(mem) - 1  # at which turn was the last number previously seen
+last = mem[lastseen]
+# mem.pop()
+for turn in range(len(seen), 30000000 - 1):
+    seen[last] = turn
+    # mem.append(last)
+    if last in seen:  # already seen number, next is diff with last seen turn
       next = turn - lastseen
     else:  # first time this number, next is zero. preserve last seen 0 turn
       next = 0
-      lastzeroturn = seen[0]
-    lastseen = seen[next]
-    seen[next] = turn
+    lastseen = turn if next == last else seen[next] if next in seen else turn + 1
     last = next
 print(last)
