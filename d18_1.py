2b9cdebe663ff61c8c233ff51a6f577a2d66e0e9
Ervin Hülp <ervin.hylp@gmail.com>
2020-12-19 16:46:08 +0200

    ver1.18.1 wip

--- d18_1.py
+++ d18_1.py
@@ -1,30 +1,37 @@
 import numpy as np
-exps=['1 + ( 2 * 3 ) + ( 4 * ( 5 + 6 ) )'.split(' ')]
+#exps=['1 + ( 2 * 3 ) + ( 4 * ( 5 + 6 ) )'.split(' ')]
+exps=['1 + 2 * 3 + ( 4 * ( 5 + 6 ) )'.split(' ')]
 
 def evalexp(inexp):
   print('eval:', inexp)
-  icnt = 0
+  argcnt = 0
+  arg = [0,0,0]
   op = ''
+  nest = 1
+  accu = 0
+  result = ''
   for i in range(len(inexp)):
     if ('0' <= inexp[i][0] and inexp[i][0] <= '9'):
-      print('int:', inexp[i])
-      icnt += 1
-      if icnt == 2:
-        if op == '+':
-          print('return: ',x + int(inexp[i]))
-          return str(x + int(inexp[i]))
-        if op == '*':
-          print('return: ',x * int(inexp[i]))
-          return str(x * int(inexp[i]))
-      x = int(inexp[i])
-    if inexp[i][0] == '+':
+      argcnt += 1
+      arg[argcnt] = int(inexp[i])
+      if argcnt == 1:
+        print('int:(', argcnt, ') =', inexp[i])
+      else:
+        result = ' res =' + str(arg[1]) + op + str(arg[2])
+        print(result)
+        #return inexp[i]
+    elif inexp[i][0] == '+':
       op = '+'
       print('op:', op)
-    if inexp[i][0] == '*':
-      op = '*'
-      print('op:', op)
-    if inexp[i][0] == '(':
-      nest = 1
+      if argcnt == 2:
+        print('return:+ ',[str(accu + int(inexp[i])), evalexp(inexp[i + 1:])])
+        return [str(accu + int(inexp[i])), evalexp(inexp[i + 1:])]
+    elif inexp[i][0] == '*':
+      print('op:*', op)
+      if argcnt == 2:
+        print('return:* ',[str(accu + int(inexp[i])), evalexp(inexp[i + 1:])])
+        return [str(accu * int(inexp[i])), evalexp(inexp[i + 1:])]
+    elif inexp[i][0] == '(':
       for j in range(i + 1,len(inexp)):
         if inexp[j][0] == '(':
           nest += 1
