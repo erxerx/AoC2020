f2ff8c4d79b1bd63a0aef9740058c9d311bb0e7a
Ervin Hülp <ervin.hylp@gmail.com>
2020-12-26 07:17:17 +0200

    fixed problems

--- d20_1.py
+++ d20_1.py
@@ -1,64 +1,66 @@
-from itertools import permutations, product, combinations
 from collections import defaultdict
+from itertools import permutations, product
 from math import sqrt
 
 edges = defaultdict(list)
 
 with open('d20.in1', 'r') as f:
     content = f.read()
-lcontent = content.split('\n\n')
-tiles = {int(x.split(':')[0][5:]): x.split('\n')[1:11] for x in lcontent}
-tlen = 10
+rec = content.split('\n\n')
+tiles = {int(x.split(':')[0][5:]): x.split('\n')[1:11] for x in rec}
+tile_len = 10
 glen = int(sqrt(len(tiles)))
 grid = [[(0, 0)] * glen] * glen
 
 
-def edge(tilenum, edgenum):
-    if edgenum == 0: return tiles[tilenum][0]
-    if edgenum == 1: return ''.join([tiles[tilenum][x][tlen - 1] for x in range(tlen)])
-    if edgenum == 2: return tiles[tilenum][tlen - 1]
-    if edgenum == 3: return ''.join([tiles[tilenum][x][0] for x in range(tlen)])
-    if edgenum == 4: return tiles[tilenum][tlen - 1]
-    if edgenum == 5: return ''.join([tiles[tilenum][tlen - x - 1][tlen - 1] for x in range(tlen)])
-    if edgenum == 6: return tiles[tilenum][0]
-    if edgenum == 7: return ''.join([tiles[tilenum][tlen - x - 1][0] for x in range(tlen)])
+def edge(tile_num, edge_num):
+    if edge_num == 0: return tiles[tile_num][0]
+    if edge_num == 1: return ''.join([tiles[tile_num][x][tile_len - 1] for x in range(tile_len)])
+    if edge_num == 2: return tiles[tile_num][tile_len - 1]
+    if edge_num == 3: return ''.join([tiles[tile_num][x][0] for x in range(tile_len)])
+    if edge_num == 4: return tiles[tile_num][tile_len - 1]
+    if edge_num == 5: return ''.join([tiles[tile_num][tile_len - x - 1][tile_len - 1] for x in range(tile_len)])
+    if edge_num == 6: return tiles[tile_num][0]
+    if edge_num == 7: return ''.join([tiles[tile_num][tile_len - x - 1][0] for x in range(tile_len)])
 
 
-def chkgrid(grid):
+def chk_grid():
     return True
-    for y in range(glen - 1):
-        for x in range(glen - 1):
-            print(x, y)
+    # for y in range(glen - 1):
+    #    for x in range(glen - 1):
+    #        print(x, y)
 
 
-def solvegrid(grid, available):
-    if not chkgrid(grid): return False
+def solve_grid(available):
+    global grid
+    if not chk_grid(): return False
     if len(available) == 0: return True
-    trytile = available.pop()
-    for orientation in range(8):
-        grid.append((trytile, orientation))
-        if solvegrid(grid, available):
-            print('YESS')
+    try_tile = available.pop()
+    print('try_tile', try_tile)
+    for loc_orientation in range(8):
+        # ... grid.append((try_tile, loc_orientation))
+        if solve_grid(available):
+            print('YES')
         else:
-            print('NOOO')
+            print('NO')
 
 
-for tile in tiles.keys():
-    for orientation in range(8):
-        # print(edge(tile, orientation), tile, orientation)
-        edges[edge(tile, orientation)].append((tile, orientation))
+if __name__ == '__main__':
+    for tile in tiles.keys():
+        for orientation in range(8):
+            # print(edge(tile, orientation), tile, orientation)
+            edges[edge(tile, orientation)].append((tile, orientation))
 
-print(solvegrid([], list(tiles.keys())))
-
-for tile in tiles.keys():
-    fit = 0
-    for orientation in range(8):
-        fit += len(edges[edge(tile, orientation)])
-    print(tile, fit)
-
-orientations = list([range(4)] * glen * glen)
-a = 0
-for perm in zip(product(permutations(tiles.keys()), product(*orientations))):
-    a += 1
-
-print(a)
+    for tile in tiles.keys():
+        fit = 0
+        for orientation in range(8):
+            fit += len(edges[edge(tile, orientation)])
+        print(tile, fit)
+    
+    orientations = list([range(4)] * glen * glen)
+    a = 0
+    for perm in zip(product(permutations(tiles.keys()), product(*orientations))):
+        a += 1
+    
+    print(a)
+    print(list(tiles.keys()))
