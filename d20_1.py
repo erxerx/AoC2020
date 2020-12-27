06406ad3a2647a9fb7c285317a04b252728a8a80
Ervin Hülp <ervin.hylp@gmail.com>
2020-12-27 19:40:43 +0200

    wip

--- d20_1.py
+++ d20_1.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import product
 from math import sqrt
 # globals
 edges = defaultdict(list)
@@ -18,16 +19,37 @@ def edge(tile_num, orientation, edge_num):  # return tile edge
 
 
 def ok_tile(tile_pos, tile_num, orientation):
-    if tile_num >= glen:  # from 2nd row check that tile's upper edge matches previous row's lower edge
-        if edge(tile_num, orientation, 0) != edge(grid[tile_pos - glen], orientation, 2): return False
-    if tile_num % glen:  # from 2nd col check that tile's right edge matches previous col's left edge
-        if edge(tile_num, orientation, 1) != edge(tile_pos - 1, orientation, 3): return False
+    if tile_pos >= glen:  # from 2nd row check that tile's upper edge matches previous row's lower edge
+        if edge(tile_num, orientation, 0) != edge(*grid[tile_pos - glen], 2): return False
+    if tile_pos % glen:  # from 2nd col check that tile's left edge matches previous col's right edge
+        if edge(tile_num, orientation, 3) != edge(*grid[tile_pos - 1], 1): return False
     return True
 
 
 def solve_grid(start_pos):
-    for tile in range(start_pos, len(tiles)):
-        print(ok_tile(start_pos, tile, 0))
+    for tile, orientation in available:
+        print('at', start_pos, 'trying', tile, orientation)
+        if not ok_tile(start_pos, tile, orientation): continue  # does not fit, take next
+        grid[start_pos] = (tile, orientation)  # let's try it out
+        available.remove((tile, 0))  # remove used one from available list and continue search
+        available.remove((tile, 1))  # remove all orientations
+        if start_pos == len(tiles): return True  # solution found if all in place
+        if solve_grid(start_pos + 1):
+            return True
+        else:
+            available.add((tile, 0))  # if can not use, add back to available set
+            available.add((tile, 1))  # if can not use, add back to available set
+    grid[start_pos] = (0, 0)
+    return False  # dead end, backtrack
+
+    # if not available: return False  # dead end
+    # elif tile_pos < glen:  # set of tiles and orientations that match prev tile's right edge
+    #     available = [x for x in edges[edge(*grid[tile_pos - 1], 1)]]
+    # if
+    #
+    # print('available:', len(list(available)))
+    # # print(ok_tile(tile_pos, 1427, 0))
+    # [ x for x in edges['###.##.#..'] if x[2] == 2 ]
     # if not ok_tile(): return False
     # if len(available) == 0: return True
     # try_tile = available.pop()
@@ -38,7 +60,7 @@ def solve_grid(start_pos):
     #         print('YES')
     #     else:
     #         print('NO')
-    return True
+    # return True
 
 
 if __name__ == '__main__':
@@ -48,23 +70,24 @@ if __name__ == '__main__':
     tiles = {int(x.split(':')[0][5:]): x.split('\n')[1:11] for x in rec}
     tile_len = 10
     glen = int(sqrt(len(tiles)))  # grid side length
-    grid = [(2311, 1)] * len(tiles)
-
-    solve_grid(4)
+    grid = [(0, 0)] * len(tiles)
 
     for one_tile in tiles.keys():
-        for oriented in range(4):
+        for enum in range(4):
             # print(edge(tile, orientation), tile, orientation)
-            edges[edge(one_tile, 0, oriented)].append((one_tile, oriented))
-            edges[edge(one_tile, 1, oriented)].append((one_tile, oriented))
+            edges[edge(one_tile, 0, enum)].append((one_tile, 0, enum))
+            edges[edge(one_tile, 1, enum)].append((one_tile, 1, enum))
 
     for one_tile in tiles.keys():
         fit = 0
-        for oriented in range(4):
-            fit += int(len(edges[edge(one_tile, 0, oriented)]) / 2)
-            fit += int(len(edges[edge(one_tile, 1, oriented)]) / 2)
-        print(one_tile, fit)
-    
+        for enum in range(4):
+            fit += int(len(edges[edge(one_tile, 0, enum)]) / 2)
+            fit += int(len(edges[edge(one_tile, 1, enum)]) / 2)
+        # print(one_tile, fit)
+
+    available = set(product(tiles.keys(), [0, 1]))  # all tiles and orientations could match
+    print(solve_grid(0))
+
     # orientations = list([range(4)] * glen * glen)
     # a = 0
     # for perm in zip(product(permutations(tiles.keys()), product(*orientations))):
