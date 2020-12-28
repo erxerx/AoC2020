3b59af62927e3c46086587bd6c8d38742b1bf383
Ervin Hülp <ervin.hylp@gmail.com>
2020-12-28 13:01:29 +0200

    ver1.120 part1 ok

--- d20_1.py
+++ d20_1.py
@@ -1,94 +1,88 @@
-from collections import defaultdict
-from itertools import product
 from math import sqrt
-# globals
-edges = defaultdict(list)
 
 
 def edge(tile_num, orientation, edge_num):  # return tile edge
+    n = tiles[tile_num][0]
+    e = ''.join([tiles[tile_num][x][tile_len - 1] for x in range(tile_len)])
+    s = tiles[tile_num][tile_len - 1]
+    w = ''.join([tiles[tile_num][x][0] for x in range(tile_len)])
     if orientation == 0:
-        if edge_num == 0: return tiles[tile_num][0]
-        if edge_num == 1: return ''.join([tiles[tile_num][x][tile_len - 1] for x in range(tile_len)])
-        if edge_num == 2: return tiles[tile_num][tile_len - 1]
-        if edge_num == 3: return ''.join([tiles[tile_num][x][0] for x in range(tile_len)])
-    else:
-        if edge_num == 0: return tiles[tile_num][tile_len - 1]
-        if edge_num == 1: return ''.join([tiles[tile_num][tile_len - x - 1][tile_len - 1] for x in range(tile_len)])
-        if edge_num == 2: return tiles[tile_num][0]
-        if edge_num == 3: return ''.join([tiles[tile_num][tile_len - x - 1][0] for x in range(tile_len)])
+        if edge_num == 0: return n
+        if edge_num == 1: return e
+        if edge_num == 2: return s
+        if edge_num == 3: return w
+    elif orientation == 1:
+        if edge_num == 0: return w[::-1]
+        if edge_num == 1: return n
+        if edge_num == 2: return e[::-1]
+        if edge_num == 3: return s
+    elif orientation == 2:
+        if edge_num == 0: return s[::-1]
+        if edge_num == 1: return w[::-1]
+        if edge_num == 2: return n[::-1]
+        if edge_num == 3: return e[::-1]
+    elif orientation == 3:
+        if edge_num == 0: return e
+        if edge_num == 1: return s[::-1]
+        if edge_num == 2: return w
+        if edge_num == 3: return n[::-1]
+    elif orientation == 4:
+        if edge_num == 0: return n[::-1]
+        if edge_num == 1: return w
+        if edge_num == 2: return s[::-1]
+        if edge_num == 3: return e
+    elif orientation == 5:
+        if edge_num == 0: return e[::-1]
+        if edge_num == 1: return n[::-1]
+        if edge_num == 2: return w[::-1]
+        if edge_num == 3: return s[::-1]
+    elif orientation == 6:
+        if edge_num == 0: return s
+        if edge_num == 1: return e[::-1]
+        if edge_num == 2: return n
+        if edge_num == 3: return w[::-1]
+    elif orientation == 7:
+        if edge_num == 0: return w
+        if edge_num == 1: return s
+        if edge_num == 2: return e
+        if edge_num == 3: return n
 
 
 def ok_tile(tile_pos, tile_num, orientation):
     if tile_pos >= glen:  # from 2nd row check that tile's upper edge matches previous row's lower edge
-        if edge(tile_num, orientation, 0) != edge(*grid[tile_pos - glen], 2): return False
+        if edge(tile_num, orientation, 0) != edge(grid_tiles[tile_pos - glen], grid_orientations[tile_pos - glen], 2):
+            return False
     if tile_pos % glen:  # from 2nd col check that tile's left edge matches previous col's right edge
-        if edge(tile_num, orientation, 3) != edge(*grid[tile_pos - 1], 1): return False
+        if edge(tile_num, orientation, 3) != edge(grid_tiles[tile_pos - 1], grid_orientations[tile_pos - 1], 1):
+            return False
     return True
 
 
 def solve_grid(start_pos):
-    for tile, orientation in available:
-        print('at', start_pos, 'trying', tile, orientation)
-        if not ok_tile(start_pos, tile, orientation): continue  # does not fit, take next
-        grid[start_pos] = (tile, orientation)  # let's try it out
-        available.remove((tile, 0))  # remove used one from available list and continue search
-        available.remove((tile, 1))  # remove all orientations
-        if start_pos == len(tiles): return True  # solution found if all in place
-        if solve_grid(start_pos + 1):
-            return True
-        else:
-            available.add((tile, 0))  # if can not use, add back to available set
-            available.add((tile, 1))  # if can not use, add back to available set
-    grid[start_pos] = (0, 0)
+    for tile in tiles.keys():
+        if tile in grid_tiles: continue
+        for orientation in range(8):
+            if not ok_tile(start_pos, tile, orientation): continue  # does not fit, take next
+            grid_tiles[start_pos] = tile
+            grid_orientations[start_pos] = orientation  # let's try it out
+            print('at', start_pos, 'trying', tile, orientation)
+            if start_pos == len(tiles) - 1: return True  # solution found if all in place
+            if solve_grid(start_pos + 1): return True
+    grid_tiles[start_pos] = 0
     return False  # dead end, backtrack
-
-    # if not available: return False  # dead end
-    # elif tile_pos < glen:  # set of tiles and orientations that match prev tile's right edge
-    #     available = [x for x in edges[edge(*grid[tile_pos - 1], 1)]]
-    # if
-    #
-    # print('available:', len(list(available)))
-    # # print(ok_tile(tile_pos, 1427, 0))
-    # [ x for x in edges['###.##.#..'] if x[2] == 2 ]
-    # if not ok_tile(): return False
-    # if len(available) == 0: return True
-    # try_tile = available.pop()
-    # print('try_tile', try_tile)
-    # for loc_orientation in range(8):
-    #     # ... grid.append((try_tile, loc_orientation))
-    #     if solve_grid(available):
-    #         print('YES')
-    #     else:
-    #         print('NO')
-    # return True
 
 
 if __name__ == '__main__':
-    with open('d20.in1', 'r') as f:
+    with open('d20.in', 'r') as f:
         content = f.read()
     rec = content.split('\n\n')
     tiles = {int(x.split(':')[0][5:]): x.split('\n')[1:11] for x in rec}
-    tile_len = 10
+    values_view = tiles.values()
+    value_iterator = iter(values_view)
+    first_value = next(value_iterator)
+    tile_len = len(first_value)
     glen = int(sqrt(len(tiles)))  # grid side length
-    grid = [(0, 0)] * len(tiles)
-
-    for one_tile in tiles.keys():
-        for enum in range(4):
-            # print(edge(tile, orientation), tile, orientation)
-            edges[edge(one_tile, 0, enum)].append((one_tile, 0, enum))
-            edges[edge(one_tile, 1, enum)].append((one_tile, 1, enum))
-
-    for one_tile in tiles.keys():
-        fit = 0
-        for enum in range(4):
-            fit += int(len(edges[edge(one_tile, 0, enum)]) / 2)
-            fit += int(len(edges[edge(one_tile, 1, enum)]) / 2)
-        # print(one_tile, fit)
-
-    available = set(product(tiles.keys(), [0, 1]))  # all tiles and orientations could match
+    grid_tiles = [0] * len(tiles)
+    grid_orientations = [0] * len(tiles)
     print(solve_grid(0))
-
-    # orientations = list([range(4)] * glen * glen)
-    # a = 0
-    # for perm in zip(product(permutations(tiles.keys()), product(*orientations))):
-    #     a += 1
+    print(grid_tiles[0] * grid_tiles[glen - 1] * grid_tiles[len(tiles) - glen] * grid_tiles[len(tiles) - 1])
