24e9be7fa10c92b8366dc7bc7f03b4d676b4700b
erx <ervin.hylp@gmail.com>
2021-01-04 20:40:43 +0200

    ver1.22 part2 ok

--- d22.py
+++ d22.py
@@ -1,43 +1,38 @@
-seen = {}
-
-
-def play(deck1, deck2):
-    global seen
+def play(deck1, deck2):  # return True if player1 wins
+    seen = set()
     while deck1 and deck2:
-        if deck1 and str(deck1) in seen:
-            seen = {}
+        if str(deck1 + [0] + deck2) in seen:
             return True
-        seen[str(deck1)] = 1
+        seen.add(str(deck1 + [0] + deck2))
         p1 = deck1.pop(0)
         p2 = deck2.pop(0)
         if p1 <= len(deck1) and p2 <= len(deck2):
             if play(deck1[:p1], deck2[:p2]):
                 deck1.append(p1)
                 deck1.append(p2)
-                # return True
             else:
                 deck2.append(p2)
                 deck2.append(p1)
-                # return False
         elif p1 > p2:
             deck1.append(p1)
             deck1.append(p2)
         else:
             deck2.append(p2)
             deck2.append(p1)
-    seen = {}
     return deck2 == []
 
 
-with open('d22.in1', 'r') as f:
+with open('d22.in', 'r') as f:
     content = f.read()
 ll = content.split('\n\n')
 players = [x.split(':')[0] for x in ll]
 cards = [[int(y) for y in x.split(':\n')[1].split('\n')] for x in ll]
-print(play(cards[0], cards[1]))
-cc = len(cards[1])
-winner = 0
-for i in cards[1]:
-    winner += cc * i
+print(p1win := play(cards[0], cards[1]))
+cc = len(cards[0])
+score = 0
+for i in cards[0]:
+    score += cc * i
     cc -= 1
-print(winner)
+print(score)
+# 32427 too low
+# 32769 OK
