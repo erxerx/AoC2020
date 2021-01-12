3472a8595adfc2696902c475943017fa230bf6ab
Ervin Hülp <ervin.hylp@gmail.com>
2021-01-12 21:04:38 +0200

    ver1.24 part2 ok

--- d24.py
+++ d24.py
@@ -1,6 +1,7 @@
+from time import time
 import operator
-
-with open('d24.in1', 'r') as f:
+start = time()
+with open('d24.in', 'r') as f:
     content = f.read()
 routes = content.split('\n')
 # idea from https://stackoverflow.com/questions/2459402/hexagonal-grid-coordinates-to-pixel-coordinates
@@ -12,28 +13,34 @@ for route in routes:
     for move in route:
         r, g, b = tuple(map(sum, zip((r, g, b), h_move[int(move)])))
     if (r, g, b) in h_grid:
-        h_grid[(r, g, b)] = 1 - h_grid[(r, g, b)]
+        h_grid.pop((r, g, b))
+        blacks -= 1
     else:
         h_grid[(r, g, b)] = 1
-    blacks += (2 * h_grid[(r, g, b)] - 1)  # to convert 0 or 1 to -1 or +1
+        blacks += 1
 # print(blacks)
 # part1: 282
-n_state = set()
-for i in h_grid:
-    if i: n_state.add(i)
-for day in range(5):
-    h_state = n_state
-    n_state = set()
+for day in range(100):
+    n_grid = {}
     blacks = 0
-    print(len(h_state), h_state)
-    for current in h_state:
+    for current in h_grid:
         for move in h_move:
-            nearby = 0
             look = tuple(map(operator.add, current, move))
-            if look in n_state: continue
-            for tile in h_state:
-                if tuple(map(operator.sub, tile, look)) in h_move[1:]: nearby += 1
-            if ((look in h_state) and (0 < nearby) and (nearby < 3)) or \
-                    ((look not in h_state) and nearby == 2):
-                n_state.add(look)
+            if look in n_grid: continue
+            nearby = 0
+            for tile in h_grid:
+                if tuple(map(operator.sub, look, tile)) in h_move[1:]: nearby += 1
+            if ((look in h_grid and h_grid[look]) and (0 < nearby) and (nearby < 3)) or \
+                    ((look not in h_grid or not h_grid[look]) and nearby == 2):
+                n_grid[look] = 1
                 blacks += 1
+            else:
+                n_grid[look] = 0
+    h_grid = {}
+    for i in n_grid:
+        if n_grid[i]: h_grid[i] = 1
+    print(day+1, len(h_grid))
+print(f"Time : {time() - start} seconds")
+# part2: 100 3445
+# Time : 857.372636795044 seconds (ver1)
+# Time : 372.2061810493469 seconds (ver2)
